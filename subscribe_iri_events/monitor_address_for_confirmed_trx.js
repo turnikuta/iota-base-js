c1f0663c5e5560543c377f6546b579dcc0581ca3
turnikuta <turnikuta@mail.com>
2019-05-17 10:07:41 +0200

    subscribe_iri_events added

---
+++ subscribe_iri_events/monitor_address_for_confirmed_trx.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const core = require('@iota/core');
+const zmq = require('zeromq-stable');
+
+const SOCK_PROVIDER = "tcp://zmq.devnet.iota.org:5556";
+const sock = zmq.socket('sub');
+sock.connect(SOCK_PROVIDER);
+
+const PROVIDER = "https://nodes.devnet.iota.org:443";
+const iota = core.composeAPI({ provider: PROVIDER });
+
+// get a address 
+const addr = process.argv[2];
+if (addr == undefined || !addr.match(/^[A-Z9]*$/)){
+  console.log("Please provide the address you want to monitor! (Trytes)");
+  process.exit();
+}
+
+//sock.subscribe('tx');
+sock.subscribe(addr);
+
+sock.on('message', msg => {
+  const data = msg.toString().split(' '); 
+  // [0] = address
+  // [1] = trx hash
+  // [2] = milestone index
+  iota.getTransactionObjects([data[1]])  // array with trx hashes
+  .then(trxObjects => {
+    trxObjects.map(trxData => {
+      console.log(`Transaction: ${trxData.hash}  Value: ${trxData.value}`);
+    })
+  })
+})
